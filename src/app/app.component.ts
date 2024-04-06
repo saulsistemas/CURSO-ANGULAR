d778fa4ed6d55449544c472e6e70d84700a1237a
Sandro Santamaria Ramos <R000001@inmobiliariahuertas.local>
2024-04-05 23:51:52 -0500

    Iniciando proyecto

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'PrimerosPasos';
+
+  public title:string = 'PrimerosPasos';
+  public contador:number =10;
 }
