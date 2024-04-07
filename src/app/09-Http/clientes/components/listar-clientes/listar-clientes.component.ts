438819adf1c4ad4c887036b0a5cca900cebd938b
Sandro Santamaria Ramos <R000001@inmobiliariahuertas.local>
2024-04-07 11:33:29 -0500

    Listando clientes

--- src/app/09-Http/clientes/components/listar-clientes/listar-clientes.component.ts
+++ src/app/09-Http/clientes/components/listar-clientes/listar-clientes.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ClientesService } from '../../services/clientes.service';
+import { Cliente } from '../../interfaces/cliente.interface';
 
 @Component({
   selector: 'app-listar-clientes',
   templateUrl: './listar-clientes.component.html',
-  styleUrl: './listar-clientes.component.css'
 })
-export class ListarClientesComponent {
+export class ListarClientesComponent implements OnInit{
+
+  public clientes:Cliente[] = [];
+  constructor(
+    private clientesService:ClientesService
+  ){}
+ ngOnInit(): void {
+   this.clientesService.obtenerCliente()
+      .subscribe((res:any)=>{
+        this.clientes = res;
+        console.log(this.clientes);
+      }, (err:any)=>{
+        console.log(err);
+
+      })
+ }
 
 }
--- src/app/09-Http/clientes/services/clientes.service.ts
+++ src/app/09-Http/clientes/services/clientes.service.ts
@@ -1,9 +1,23 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import {  map } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ClientesService {
 
-  constructor() { }
+  private baseUrl: string = 'http://localhost:3000';
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  obtenerCliente(){
+    return this.http.get(this.baseUrl)
+      .pipe(
+        map((data: any) => {
+          return data;
+        })
+      )
+  }
 }
